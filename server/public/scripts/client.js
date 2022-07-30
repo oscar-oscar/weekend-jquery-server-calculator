ae257fc4374637ce5d01093b2bc1dc067a8ab768
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-30 11:11:48 -0500

    created click handlers and changed button id ffrom sum to equation-button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,18 @@ function readyNow() {
     console.log('jQuery ready!');
 
 
+$('#addition-button').on('click', addNumber);
+
+$('#subtract-button').on('click', subtractNumber);
+
+$('#multiply-button').on('click', multipyNumber);
+
+$('#divide-button').on('click', divideNumber);
+
+$('#equation-button').on('click', enterEquation);
+
+$('#clear-button').on('click', clearEquation);
+
+
 
 }
