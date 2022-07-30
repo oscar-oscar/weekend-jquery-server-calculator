3085ace04f4708c83e529996114903b0d45b40dd
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-30 11:04:16 -0500

    fied app.use url encoded line syntax

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5005;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded());
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
